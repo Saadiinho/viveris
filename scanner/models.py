a9e786a2d2f09bef06a075d41de7572999d32af2
saadiinho <saadrafiqul93380@gmail.com>
2025-01-16 16:20:28 +0200

    Add trash model and Endpoints

--- scanner/models.py
+++ scanner/models.py
@@ -17,3 +17,13 @@ class UserScore(models.Model):
 
     def __str__(self):
         return f"User {self.user_id} - Score: {self.score}"
+    
+
+class Bin(models.Model):
+    latitude = models.FloatField()  # Utilisez FloatField pour les coordonnées
+    longitude = models.FloatField()  # Corrigez l'orthographe et utilisez FloatField
+    types = models.JSONField()  # Utilisez JSONField pour stocker une liste ou un objet JSON
+    comment = models.CharField(max_length=255, blank=True, null=True)  # Ajoutez max_length
+
+    def __str__(self):
+        return f"Bin at ({self.latitude}, {self.longitude})"
